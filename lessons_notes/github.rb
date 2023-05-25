2d8ad0f96118cdf6c426721bfa5b81940d9254ee
Leandran <41592532+Leandran@users.noreply.github.com>
2023-05-26 01:37:17 +0200

    feat: Added Github lesson notes

---
+++ lessons_notes/github.rb
@@ -0,0 +1,16 @@
+
+# Explain the difference between remote and local repositories?
+# A local git repo is stored on your computer and a remote git repo is in the cloude where collaborators can access it through the web. 
+
+# What do you already know about Git/GitHub?
+# Git is a version control system that allows developers to save, manage, keep track of and remove changes in their code.
+# Github is website that provides hosting and sharing for repositories.
+
+# Why do we use Git/GitHub?
+# Git allows developers and companies alike to track and manage source control changes while Github provides shared location for hosting git projects.
+
+# How do you create a git repository locally?
+# Create a folder for which your project will be in and initialize it by typing "git init".
+
+# What should be included in a git commit message?
+# The type of commit and a brief description. example = feat: Added a new class (feature).
