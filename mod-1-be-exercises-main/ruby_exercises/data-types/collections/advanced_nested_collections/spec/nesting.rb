98e2da5c2c1f4a8e8acdffb939cef0e5195fcf7b
Leandran <41592532+Leandran@users.noreply.github.com>
2023-05-28 21:00:07 +0200

    Update folder names

--- mod-1-be-exercises-main/ruby_exercises/data-types/collections/advanced_nested_collections/spec/nesting.rb
+++ mod-1-be-exercises-main/ruby_exercises/data-types/collections/advanced_nested_collections/spec/nesting.rb
@@ -6,7 +6,7 @@ def stores
         'Zach',
         'Samantha'
       ],
-      dishes: [
+      dishes:[
         {
           name: 'Risotto',
           ingredients: [
@@ -24,7 +24,7 @@ def stores
           ],
           price: 15
         }
-      ]
+      ] 
     },
     dennys: {
       employees: [
@@ -83,3 +83,40 @@ def stores
     }
   } 
 end
+
+# p stores[:dennys][:dishes][0][:ingredients]
+
+# p stores[:olive_garden][:dishes][0][:price]
+
+# p stores[:macdonalds][:dishes][0][:ingredients]
+
+# p stores.keys
+
+# names =[]
+# stores[:olive_garden][:dishes].each do |item|
+#   names << item[:name]
+# end
+# p names 
+
+# employees = []
+# stores.each do |k, v|  
+#   employees << v[:employees]  
+# end
+# p employees.flatten 
+
+# ingredients_list = []
+# stores.each do |store, item|
+#   item[:dishes].each do |dish|
+#     ingredients_list.concat(dish[:ingredients])
+#   end
+# end
+# p ingredients_list#.flatten 
+
+# total_price = 0
+# stores[:olive_garden][:dishes].each do |item|
+#   total_price += item[:price]
+# end
+# p total_price
+
+olive_garden_menu = {}
+stores
--- vehicle_boolean-main/lib/vehicle_analysis.rb
+++ vehicle_boolean-main/lib/vehicle_analysis.rb
@@ -2,32 +2,56 @@
 
 class VehicleAnalysis
 
-  def analyze(vehicle)
-    if vehicle.car?
-      if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
-        puts "Vehicle has four wheels "
-        if vehicle.four_wheel_drive?
-          puts "with four wheel drive"
-        else
-          puts "with two wheel drive"
-        end
-      end
-    elsif vehicle.tractor?
-      puts "Vehicle has four wheels "
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
-    elsif vehicle.pickup?
-      puts "Vehicle has four wheels "
-      if vehicle.four_wheel_drive?
-        puts "with four wheel drive"
-      else
-        puts "with two wheel drive"
-      end
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
+  def analyze(vehicle)    
+
+    if vehicle.car? && if vehicle.four_wheel_drive?
+      puts "Vehicle has four wheels with four wheel drive"        
+    else
+      puts "Vehicle has four wheels with two wheel drive"
     end
+      
+    if vehicle.tractor? && vehicle.big_back_wheels?      
+      puts "Vehicle has four wheels with big wheels in the back"
+    end
+
+    if vehicle.pickup? && vehicle.four_wheel_drive?
+      puts "Vehicle has four wheels with four wheel drive"        
+    else
+      puts "Vehicle has four wheels with two wheel drive"
+    end
+
+    if vehicle.big_back_wheels?
+      puts "with big wheels in the back"
+    end
+    
   end
+
+  # def analyze(vehicle)
+  #   if vehicle.car?
+  #     if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
+  #       puts "Vehicle has four wheels "
+  #       if vehicle.four_wheel_drive?
+  #         puts "with four wheel drive"
+  #       else
+  #         puts "with two wheel drive"
+  #       end
+  #     end
+  #   elsif vehicle.tractor?
+  #     puts "Vehicle has four wheels "
+  #     if vehicle.big_back_wheels?
+  #       puts "with big wheels in the back"
+  #     end
+  #   elsif vehicle.pickup?
+  #     puts "Vehicle has four wheels "
+  #     if vehicle.four_wheel_drive?
+  #       puts "with four wheel drive"
+  #     else
+  #       puts "with two wheel drive"
+  #     end
+  #     if vehicle.big_back_wheels?
+  #       puts "with big wheels in the back"
+  #     end
+  #   end
+  # end
 
 end
